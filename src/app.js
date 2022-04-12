327fd383697903c92a4a7d1d4f7409d2fe97efa6
kalimbak <kevin.kalimba54@gmail.com>
2022-04-12 12:40:38 +0200

    dotenv
    dotenv
    
    configured
    
    dotenv
    
    dotenv

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './documentation/index';
 
+
+
 const app = express();
+
+
 
 app.get('/api/v1', (req, res) => {
   res.status(200).json({
-    message: 'Welcome to Dominators-Phantom-API!',
+    message: "Welcome to Dominators-Phantom-API!",
   });
 });
+
 
 app.use(
   '/api-docs',
