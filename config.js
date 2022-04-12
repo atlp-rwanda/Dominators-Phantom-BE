83fc24b9d7466f54face74c8dcc42b6b0cc9610c
kalimbak <kevin.kalimba54@gmail.com>
2022-04-12 13:54:23 +0200

    dotenv

--- config.js
+++ config.js
@@ -4,15 +4,20 @@ dotenv.config()
 
 const config= {
     development: {
-        port: process.env.DEVPORT,
+        port: process.env.PORT,
         DB: process.env.DEVDB,
         JWT_SECRET: process.env.JWT_SECRET
     },
     test: {
-        port: process.env.TESTPORT,
+        port: process.env.TEST_PORT,
         DB: process.env.TESTDB,
         JWT_SECRET: process.env.JWT_SECRET
-    }
+    },
+    production: {
+        port: process.env.PORT,
+        DB: process.env.DEVDB,
+        JWT_SECRET: process.env.JWT_SECRET
+    },
 
 }
 const currentConfig = config[process.env.NODE_ENV];
