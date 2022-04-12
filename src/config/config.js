d88ba077ba01ff1feb85bd39a5400d65b3ab210f
kalimbak <kevin.kalimba54@gmail.com>
2022-04-12 12:40:38 +0200

    Translated i18next dotenv
    configured
    
    dotenv
    
    support multiple languages
    
    i18next
    
    translation
    
    i18next
    
    i18next
    
    translated
    
    final commit
    
    final commit
    
    dotnve
    
    dotenv
    
    configured
    
    dotenv
    
    support multiple languages
    
    i18next
    
    translation
    
    i18next
    
    i18next
    
    final commit
    
    multiple languages
    
    support multiple languages
    
    support multiple languages
    
    dotnve
    
    dotenv
    
    configured
    
    dotenv
    
    support multiple languages
    
    i18next
    
    translation
    
    i18next
    
    i18next
    
    translated
    
    final commit
    
    final commit
    
    dotnve
    
    dotenv
    
    configured
    
    dotenv
    
    support multiple languages
    
    i18next
    
    translation
    
    i18next
    
    i18next
    
    final commit
    
    multiple languages
    
    support multiple languages
    
    support multiple languages
    
    ch-translation

--- src/config/config.js
+++ src/config/config.js
@@ -21,5 +21,5 @@ module.exports = {
         dialect: "postgres",
         port: process.env.PORT
     },
-
+  
 }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
-import app from './app';
+import app from '../src/app';
 import config from './config/config';
 
 const currentConfig = config[process.env.NODE_ENV];
-
 const { port } = currentConfig;
-// console.log(port);
+
 
 const server = app.listen(port, () =>
   console.log(`App listening on ${port}!....`)
