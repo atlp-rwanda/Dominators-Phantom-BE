852e1f8557232601497a2a720a53a1fef02786a4
heiskwizera <kwizerageniklenon@gmail.com>
2022-05-09 10:13:20 +0200

    CRUD ROUTES Tests

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -20,39 +20,39 @@ describe('TESTING USER AUTHENTICATION', () => {
     done();
   });
 
-  // it('User login fail(message:Incorrect email or password)', (done) => {
-  //   chai
-  //     .request(server)
-  //     .post('/api/v1/users/login')
-  //     .send({
-  //       email: 'admin@mail.com',
-  //       password: 'mybrandapi',
-  //     })
-  //     .end((error, response) => {
-  //       chai.expect(response.statusCode).to.equal(401);
-  //       chai.expect(response.body).to.have.property('error');
-  //       chai
-  //         .expect(response.body.message)
-  //         .to.equal('Incorrect email or password');
-  //     });
-  //   done();
-  // });
+  it('User login fail(message:Incorrect email or password)', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/users/login')
+      .send({
+        email: 'admin@mail.com',
+        password: 'mybrandapi',
+      })
+      .end((error, response) => {
+        chai.expect(response.statusCode).to.equal(401);
+        chai.expect(response.body).to.have.property('error');
+        chai
+          .expect(response.body.message)
+          .to.equal('Incorrect email or password');
+      });
+    done();
+  });
 
-  // it('User login fail(message:Please provide email and password!)', (done) => {
-  //   chai
-  //     .request(server)
-  //     .post('/api/v1/users/login')
-  //     .send({
-  //       email: '',
-  //       password: '',
-  //     })
-  //     .end((error, response) => {
-  //       chai.expect(response.statusCode).to.equal(400);
-  //       chai.expect(response.body).to.have.property('error');
-  //       chai
-  //         .expect(response.body.message)
-  //         .to.equal('Please provide email and password!');
-  //     });
-  //   done();
-  // });
+  it('User login fail(message:Please provide email and password!)', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/users/login')
+      .send({
+        email: '',
+        password: '',
+      })
+      .end((error, response) => {
+        chai.expect(response.statusCode).to.equal(400);
+        chai.expect(response.body).to.have.property('error');
+        chai
+          .expect(response.body.message)
+          .to.equal('Please provide email and password!');
+      });
+    done();
+  });
 });
