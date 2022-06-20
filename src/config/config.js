12b409a31ab2dea97adb6d2a8c4c3bb640d64ea1
UmuhireJessie <j.umuhire@alustudent.com>
2022-06-20 12:12:35 +0200

    change database url on heroku

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
 
   development: {
     JWT_SECRET: process.env.JWT_SECRET,
-    url: process.env.HEROKU_POSTGRESQL_BLACK_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     port: process.env.PORT,
     token: process.env.ADMIN_TOKEN
